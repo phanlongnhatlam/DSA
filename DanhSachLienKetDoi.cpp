e5652cc0b5d908aa4ffbd2ee5f44c5de4b6bb062
phanlongnhatlam <154681600+phanlongnhatlam@users.noreply.github.com>
2024-07-07 15:06:44 +0700

    Add files via upload

---
+++ DanhSachLienKetDoi.cpp
@@ -0,0 +1,169 @@
+﻿#include<iostream>
+using namespace std;
+struct Node
+{
+	int data;
+	Node* pre;
+	Node* next;
+};
+// tạo một node trỏ tới null ở hai phía pre và next
+Node* MakeNode(int x) {
+	Node* tmp = new Node;
+	tmp->data = x;
+	tmp->next = NULL;
+	tmp->pre = NULL;
+	return tmp;
+}
+// In danh sách
+void InDanhSach(Node* head) {
+	while (head != NULL) {
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+// Đếm trong danh sách có bao nhiêu phần tử
+int Count(Node* head) {
+	int dem = 0;
+	while (head != NULL) {
+		head = head->next;
+		dem++;
+	}
+	return dem;
+}
+// hàm chèn đầu 
+void InsertFirst(Node *&head, int x) {
+	Node* tmp = MakeNode(x);
+	tmp->next = head;
+	if (head != NULL) {
+		head->pre = tmp;
+	}
+	head = tmp;
+}
+// hàm chèn cuối
+void InsertLast(Node*& head, int x) {
+	Node* tmp = MakeNode(x);
+	if (head == NULL) {
+		head = tmp;
+		return;
+	}
+	else
+	{
+		Node* run = head;
+		while (run->next != NULL) {
+			run = run->next;
+		}
+		run->next = tmp;
+		tmp->pre = run;
+	}
+}
+void InsertMiddle(Node *&head, int pos, int x) {
+	if (pos<0 || pos>Count(head)) {
+		cout << "Vi tri chen khong hop le !!! " << endl;
+		return;
+	}
+	if (pos == 0) {
+		InsertFirst(head,x);
+	}
+	else if (pos == Count(head)) {
+		InsertLast(head, x);
+	}
+	else
+	{
+		Node* tmp = MakeNode(x);
+		Node* run = head;
+		for (int i = 0; i < pos-1; i++) {
+			run = run->next;
+		}
+		tmp->next = run->next;
+		run->next = tmp;
+		run->next->pre = tmp;
+		tmp->pre = run;
+	}
+}
+// Xóa đầu
+int DeleteFirst(Node*& head) {
+	if (head == NULL) {
+		cout << "Danh sach rong !!! " << endl;
+		return -1;
+	}
+	else
+	{
+		Node* tmp = head;
+		int phantudaxoa = head->data;
+		head = head->next;
+		delete tmp;
+		return phantudaxoa;
+	}
+}
+// Xóa cuối
+int DeleteLast(Node*& head) {
+	if (head == NULL) {
+		cout << "Danh sach rong !!! " << endl;
+		return -1;
+	}
+	else
+	{
+		Node* chaytruoc = 0;
+		Node* chaysau = head;
+		while (chaysau->next != NULL) {
+			chaytruoc = chaysau;
+			chaysau = chaysau->next;
+		}
+		// nếu trong danh sách chỉ có một node
+		if (chaytruoc == 0) {
+			Node* tmp = head;
+			int phantudaxoa = head->data;
+			head = NULL;
+			delete tmp;
+			return phantudaxoa;
+		}
+		else
+		{
+			Node* tmp = chaysau;
+			int phantudaxoa = chaysau->data;
+			chaytruoc->next = NULL;
+			delete tmp;
+			return phantudaxoa;
+		}
+		
+	}
+}
+// Xóa giữa
+int DeleteMiddle(Node*& head, int pos) {
+	if (pos<0 || pos>Count(head)) {
+		cout << "Vi tri xoa khong hop le !!! " << endl;
+		return -1;
+	}
+	if (pos == 0) {
+		DeleteFirst(head);
+	}
+	else if (pos == Count(head)-1) {
+		DeleteLast(head);
+	}
+	else
+	{
+		Node* run = head;
+		for (int i = 0; i < pos-1 ; i++) {
+			run = run->next;
+		}
+		Node* tmp = run;
+		int phantudaxoa = tmp->data;
+		run->next = run->next->next;
+		run->next->next->pre = run;
+		return phantudaxoa;
+		delete tmp;	
+	}
+}
+int main()
+{
+	Node* head = NULL;
+	InsertFirst(head, 1);
+	InsertLast(head, 3);
+	InsertLast(head, 4);
+	InsertMiddle(head, 1, 2);
+	DeleteMiddle(head, 1);
+	InDanhSach(head);
+	system("pause");
+	return 0;
+}
---
+++ DanhSachLienKetDon.cpp
@@ -0,0 +1,199 @@
+﻿#include<iostream>
+using namespace std;
+struct Node
+{	
+	int data;
+	Node *next;
+};
+typedef struct Node* node;
+// Cấp phát động một node mới với dữ liệu là số nguyên x
+node makeNode(int x) {
+	node tmp = new Node;
+	tmp->data = x;
+	tmp->next = nullptr;
+	return tmp;
+}
+// Kiểm tra rỗng
+bool empty(node a) {
+	return a == NULL;
+}
+// Đếm có bao nhiêu node trong danh sách
+int Size(node a) {
+	int dem = 0;
+	while (a != nullptr) {
+		dem++;
+		a = a->next;// gán địa chỉ của node tiếp theo cho node hiện tại
+	}// cho node hiện tại nhảy tới node tiếp theo
+	return dem;
+}
+// Thêm một phần tử vào đầu danh sách liên kết 
+void insertFirst(node &a, int x) {
+	node tmp = makeNode(x);
+	if (a == NULL) {
+		a = tmp;
+	}
+	else 
+	{
+		tmp->next = a;
+		a = tmp;
+	}
+}
+// Thêm một phần tử vào cuối danh sách
+void insertLast(node& a, int x) {
+	node tmp = makeNode(x);
+	if (a == NULL) {
+		a = tmp;
+	}
+	else {
+		node p = a;
+		while (p->next != NULL) {
+			p = p->next;
+		}
+		p->next = tmp;
+	}
+}
+// Thêm một phần tử vào giữa danh sách  
+void insertMiddle(node& a, int x, int pos) {
+	int n = Size(a);
+	if (pos <= 0 || pos > n + 1) {
+		cout << "Vi tri chen khong hop le ! " << endl;
+	}
+	if (pos == 1) {
+		insertFirst(a,x);
+		return;
+	}
+	else if (pos == n + 1) {
+		insertLast(a, x);
+		return;
+	}
+	node p = a;
+	for (int i = 1; i < pos - 1; i++) {
+		p = p->next;
+	}
+	node tmp = makeNode(x);
+	tmp->next = p->next; 
+	p->next = tmp;
+}
+// Xóa phần tử đầu
+void deleteFirst(node& a) {
+	if (a == NULL) {
+		return;
+	}
+	a = a->next;	
+}
+// Xóa phần tử cuối
+void deleteLast(node& a) {
+	if (a == nullptr) {
+		return;
+	}
+	node truoc = NULL, sau = a;
+	while (sau->next != NULL) {
+		truoc = sau;
+		sau = sau->next;
+	}
+	if (truoc == NULL) {
+		a = NULL;
+	}
+	else
+	{
+		truoc->next = NULL;
+	}
+}
+// Xóa ở giữa 
+void deleteMiddle(node& a, int pos) {
+	// vị trí xóa không hợp lệ
+	if (pos <= 0 || pos > Size(a)) {
+		return;
+	}
+	node truoc = NULL, sau = a;
+	for (int i = 1; i < pos; i++) {
+		truoc = sau;
+		sau = sau->next;
+	}
+	if (truoc == NULL) {
+		a = a->next;
+	}
+	else
+	{
+		truoc->next = sau->next;
+	}
+}
+void in(node a) {
+	cout << "--------------------------------" << endl;
+	while (a != NULL) {
+		cout << a->data << " ";
+		a = a->next;
+	}
+	cout << endl;
+	cout << "--------------------------------" << endl;
+}
+void sapxep(node& a) {
+	//selection sort
+	for (node p = a; p->next != NULL; p = p->next) {
+		node min = p;
+		for (node q = p->next; q != NULL; q = q->next) {
+			if (q->data < min->data) {
+				min = q;
+			}
+		}
+		int tmp = min->data;
+		min->data = p->data;
+		p->data = tmp;
+	}
+}
+int main() {
+	node head = nullptr;
+	int pos;
+	int x;
+	while (true)
+	{
+		cout << "-----------------MENU------------------" << endl;
+		cout << "1. chen phan tu dau vao danh sach " << endl;
+		cout << "2. chen phan tu vao cuoi danh sach " << endl;
+		cout << "3. chen phan tu vao giua danh sach " << endl;
+		cout << "4. xoa phan tu o dau " << endl;
+		cout << "5. xoa phan tu o cuoi " << endl;
+		cout << "6. xoa phan tu o giua " << endl;
+		cout << "7. duyet danh sach lien ket " << endl;
+		cout << "8. Sap xep cac phan tu trong danh sach lien ket tang dan" << endl;
+		cout << "0. thoat " << endl;
+		cout << "---------------------------------------" << endl;
+		cout << "Lua chon : "; int luachon; cin >> luachon;
+		switch (luachon)
+		{
+		case 0:
+			return 0;
+			break;
+		case 1:
+			cout << "Nhap gia tri can chen : "; cin >> x;
+			insertFirst(head, x);
+			break;
+		case 2:
+			cout << "Nhap gia tri can chen : "; cin >> x;
+			insertLast(head, x);
+			break;
+		case 3:
+			cout << "Nhap gia tri can chen : "; cin >> x;
+			cout << "Nhap vi tri can chen : ";  cin >> pos;
+			insertMiddle(head,x,pos);
+			break;
+		case 4:
+			deleteFirst(head);
+			break;
+		case 5:
+			deleteLast(head);
+			break;
+		case 6:
+			cout << "Nhap vi tri can xoa : ";  cin >> pos;
+			deleteMiddle(head, pos);
+			break;
+		case 7:
+			in(head);
+			break;
+		case 8:
+			sapxep(head);
+		}
+	}
+	system("pause");
+	return 0;
+}
